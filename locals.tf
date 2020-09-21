b4a179383964abbfca85a6c08209e6f0e122b335
James Hodgkinson <james.hodgkinson@webcentralgroup.com.au>
2020-09-22 07:49:32 +1000

    fixed a module path issue

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
     cloudfront_s3_origin_id = "S3${var.source_hostname}"
 
-    routing_rules = templatefile("module/aws_redirector_module/RoutingRules.template",{
+    routing_rules = templatefile("${path.module}/RoutingRules.template",{
         "target_hostname" = var.target_hostname,
         "http_redirect_code" = var.http_redirect_code,
         "replace_key_prefix_with" = var.replace_key_prefix_with,
