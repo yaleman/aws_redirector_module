44f823b3a98cb0233e554eefeedbfe862ee30bbb
James Hodgkinson <james.hodgkinson@webcentralgroup.com.au>
2020-09-27 09:33:04 +1000

    updated to include some new outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output cert_domain_validation {
 output cert_status {
     value = aws_acm_certificate.cert.status
 }
+
+output cloudfront_domain_name {
+    value = aws_cloudfront_distribution.cloudfront.domain_name
+}
+
+output cloudfront_status {
+    value = aws_cloudfront_distribution.cloudfront.status
+}
